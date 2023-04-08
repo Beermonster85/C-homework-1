ad2039b9e7658e54d760a0da897af4b83b41e3e0
Kirill <kiril-shitikov@yandex.ru>
2023-04-09 02:35:54 +0300

    Docs: Done homework №1

---
+++ Program.cs
@@ -0,0 +1,150 @@
+﻿// Console.WriteLine("Введите число №1");
+// int number1 = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите число №2");
+// int number2 = Convert.ToInt32(Console.ReadLine());
+
+// if (number1 == number2 * number2)
+// {
+//     Console.WriteLine("Число 1 = квадрату числа 2");
+// }
+
+// else
+// {
+//     Console.WriteLine("число 1 не равно квадрату из числа 2");
+// }
+
+
+
+// Console.WriteLine("Введите число соответсвующее дню недели");
+// int week_number = Convert.ToInt32(Console.ReadLine());
+// if (week_number == 1)
+// {
+//     Console.WriteLine("Понедельник");
+// }
+// else if (week_number == 2)
+// {
+//     Console.WriteLine("Вторник");
+// }
+// else if (week_number == 3)
+// {
+//     Console.WriteLine("Среда");
+// }
+// else if (week_number == 4)
+// {
+//     Console.WriteLine("Четверг");
+// }
+// else if (week_number == 5)
+// {
+//     Console.WriteLine("Пятница");
+// }
+// else if (week_number == 6)
+// {
+//     Console.WriteLine("Суббота");
+// }
+// else if (week_number == 7)
+// {
+//     Console.WriteLine("Воскресенье");
+// }
+// else
+// {
+//     Console.WriteLine("Это не день недели");
+// }
+
+
+
+// Console.WriteLine("Введите любое целое число");
+
+// int number = Convert.ToInt32(Console.ReadLine());
+// int negnumber = -number;
+
+// while (number >= negnumber)
+// {
+//     Console.WriteLine(negnumber);
+//     negnumber = negnumber + 1;
+// }
+
+
+
+// Console.WriteLine("Введите трехзначное число");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// int result = number % 10;
+
+// Console.WriteLine(result);
+
+
+
+
+//Homework
+
+
+//Task №1
+
+// Console.WriteLine("Ввести первое число");
+// int number1 = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Ввести второе число");
+// int number2 = Convert.ToInt32(Console.ReadLine());
+
+// if (number1 > number2)
+// {
+//     Console.Write("max=");
+//     Console.WriteLine(number1);
+// }
+// else
+// {
+//     Console.Write("max=");
+//     Console.WriteLine(number2);
+// }
+
+
+//Task №2
+// Console.WriteLine("Ввести первое число");
+// int number1 = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Ввести второе число");
+// int number2 = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Ввести третее число");
+// int number3 = Convert.ToInt32(Console.ReadLine());
+
+// if (number1 > number2 && number1 > number3)
+// {
+//     Console.WriteLine(number1);
+// }
+// else if (number2 > number1 && number2 > number3)
+// {
+//     Console.WriteLine(number2);
+// }
+// else if (number3 > number1 && number3 > number2)
+// {
+//     Console.WriteLine(number3);
+// }
+
+
+//Task №3
+
+// Console.WriteLine("Ввести первое число");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// if (number % 2 == 0)
+// {
+//     Console.WriteLine("Да");
+// }
+// else
+// {
+//     Console.WriteLine("Нет");
+// }
+
+
+// Task 4
+
+// Console.WriteLine("Введите любое целое число");
+// int number = Convert.ToInt32(Console.ReadLine());
+// int posnumber = 2;
+// while (posnumber < number)
+// {
+//     Console.WriteLine(posnumber);
+//     posnumber = posnumber + 2;
+// }
